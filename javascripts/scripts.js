a519cae94fc6ef7864372aca2b546608423cbbbe
Owen Rumney <owen@owenrumney.co.uk>
2015-01-23 18:09:51 +0000

    add the bing wallpaper and recent updates to the hyper links

--- javascripts/scripts.js
+++ javascripts/scripts.js
@@ -33,54 +33,6 @@ jQuery.noConflict()(function($){
 });
 
 
-/* ---------- @ Portfolio -----------*/
-jQuery.noConflict()(function($){
-$(window).load(function() {   
- $(function(){
-      
-      var $container = $('#portfolio');
-      
-
-                $container.isotope({
-                  itemSelector : '.block',
-                  layoutMode : 'masonry'
-                  
-                });
-      
-      var $optionSets = $('#options .option-set'),
-          $optionLinks = $optionSets.find('a');
-
-      $optionLinks.click(function(){
-        var $this = $(this);
-        // don't proceed if already selected
-        if ( $this.hasClass('selected') ) {
-          return false;
-        }
-        var $optionSet = $this.parents('.option-set');
-        $optionSet.find('.selected').removeClass('selected');
-        $this.addClass('selected');
-  
-        // make option object dynamically, i.e. { filter: '.my-filter-class' }
-        var options = {},
-            key = $optionSet.attr('data-option-key'),
-            value = $this.attr('data-option-value');
-        // parse 'false' as false boolean
-        value = value === 'false' ? false : value;
-        options[ key ] = value;
-        if ( key === 'layoutMode' && typeof changeLayoutMode === 'function' ) {
-          // changes in layout modes need extra logic
-          changeLayoutMode( $this, options )
-        } else {
-          // otherwise, apply new options
-          $container.isotope( options );
-        }
-        
-        return false;
-      });
-      });
-      
-    });
-});
 
 /* ---------- @ Prettyphoto -----------*/
 jQuery.noConflict()(function($){
@@ -194,7 +146,7 @@ jQuery.noConflict()(function($){
 
 /* ---------- @ Scroll to Section -----------*/
 jQuery.noConflict()(function($){
-    $('.nav').onePageNav({
+    $('.navscroll').onePageNav({
     filter: ':not(.external)',
     currentClass: 'active',
     scrollOffset: 40,
